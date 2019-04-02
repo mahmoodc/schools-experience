20976f1e726d5f2b108d41abd46347f567b0383f
Richard Lynch <rlynchdev@gmail.com>
2019-04-02 07:46:06 +0100

    wip persists

--- app/forms/schools/on_boarding/phases_list.rb
+++ app/forms/schools/on_boarding/phases_list.rb
@@ -4,12 +4,32 @@ module Schools
       include ActiveModel::Model
       include ActiveModel::Attributes
 
-      attribute :primary, :boolean
-      attribute :secondary, :boolean
-      attribute :college, :boolean
+      attribute :primary, :boolean, default: false
+      attribute :secondary, :boolean, default: false
+      attribute :college, :boolean, default: false
+
+      validates :primary, inclusion: [true, false]
+      validates :secondary, inclusion: [true, false]
+      validates :college, inclusion: [true, false]
+
+      validate :at_least_one_phase_offered
+
+      def self.compose(primary, secondary, college)
+        new primary: primary, secondary: secondary, college: college
+      end
 
       def ==(other)
         other.respond_to?(:attributes) && other.attributes == self.attributes
+      end
+
+    private
+
+      def at_least_one_phase_offered
+        errors.add(:base, :no_phase_offered) unless at_least_one_phase_offered?
+      end
+
+      def at_least_one_phase_offered?
+        [primary, secondary, college].any?
       end
     end
   end
--- spec/forms/schools/on_boarding/phases_list_spec.rb
+++ spec/forms/schools/on_boarding/phases_list_spec.rb
@@ -6,4 +6,11 @@ describe Schools::OnBoarding::PhasesList, type: :model do
     it { is_expected.to respond_to :secondary }
     it { is_expected.to respond_to :college }
   end
+
+  context '#validations' do
+    it 'validates at_least_one_phase_offered' do
+      expect(described_class.new.tap(&:validate).errors[:base]).to \
+        eq ['Select at least one phase']
+    end
+  end
 end
